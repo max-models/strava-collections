0c501950f5f2a431460cafb373fcbb25f1269241
Max <max.lindqvist@ipp.mpg.de>
2026-06-13 10:24:17 +0200

    Perfecting (#75)
    * Make the elevation charts static
    
    * Added strava-collections --update-site
    
    * Removed examples/villach-to-zagreb.yml
    
    * Print moving time instead of elapsed time
    
    * Comment out the daily live data
    
    * formatting

--- src/strava_collections/main.py
+++ src/strava_collections/main.py
@@ -112,7 +112,7 @@ def generate_collection(
     collection = StravaCollection(
         name=collection_name,
         activities=parsed_activities,
-        force_update=args.force_update,
+        force_update=getattr(args, "force_update", False),
         verbose=verbose,
         description=description,
         route_gpx_file=route_gpx_file,
@@ -153,72 +153,74 @@ def generate_collection(
     map_path = path_static / mapfig_name
     map_full_name = f"{collection_filename}-map-fullscreen.html"
     map_full_path = path_static / map_full_name
-    # map_asset_paths = [
-    #     map_path,
-    #     map_full_path,
-    #     map_path.with_suffix(".png"),
-    #     path_static / f"{collection_filename}-map-thick.png",
-    # ]
 
-    elevation_extension = elevation_extension_for_backend(args.backend)
+    backend = getattr(args, "backend", "plotly")
+    elevation_extension = elevation_extension_for_backend(backend)
     elevfig_name = f"{collection_filename}-elev.{elevation_extension}"
     elev_path = path_static / elevfig_name
 
     path_collection_astro = output_dir / f"{collection_filename}.astro"
     legacy_markdown_path = output_dir / f"{collection_filename}.md"
 
-    if args.include_activity_elevation:
-        for activity in collection.activities:
-            activity.plot_elevation(
-                filepath=str(
-                    path_static
-                    / f"activity-{activity.activity_id}.{elevation_extension}"
-                ),
-                backend=args.backend,
-                verbose=verbose,
-            )
+    skip_assets = getattr(args, "skip_assets", False)
+    skip_pages = getattr(args, "skip_pages", False)
 
-    collection.plot_elevation(filepath=str(elev_path), backend=args.backend)
+    if not skip_assets:
+        if getattr(args, "include_activity_elevation", True):
+            for activity in collection.activities:
+                activity.plot_elevation(
+                    filepath=str(
+                        path_static
+                        / f"activity-{activity.activity_id}.{elevation_extension}"
+                    ),
+                    backend=backend,
+                    verbose=verbose,
+                )
 
-    # Export GPX assets for each activity
-    collection.generate_gpx_assets(path_static, verbose=verbose)
+        collection.plot_elevation(filepath=str(elev_path), backend=backend)
 
-    # Export map as html
-    collection.plot_map(
-        filepath=str(map_full_path),
-        linewidths=[4, 2],
-        height=800,
-        width_to_height=1.0,
-        places=places,
-        verbose=verbose,
-    )
+        # Export GPX assets for each activity
+        collection.generate_gpx_assets(path_static, verbose=verbose)
 
-    # We still plot the map as a PNG for thumbnails/socials
-    collection.plot_map(
-        filepath=str(map_path.with_suffix(".png")),
-        linewidths=[16, 8],
-        height=500,
-        width_to_height=1.0,
-        places=places,
-        verbose=verbose,
-    )
-    collection.plot_map(
-        filepath=str(path_static / f"{collection_filename}-map-thick.png"),
-        linewidths=[32, 16],
-        height=500,
-        width_to_height=1.0,
-        places=places,
-        verbose=verbose,
-    )
+        # Export map as html
+        collection.plot_map(
+            filepath=str(map_full_path),
+            linewidths=[4, 2],
+            height=800,
+            width_to_height=1.0,
+            places=places,
+            verbose=verbose,
+        )
 
-    collection.generate_astro(
-        filepath=str(path_collection_astro),
-        mapfig_name=mapfig_name,
-        elevfig_name=elevfig_name,
-        include_activity_elevation=args.include_activity_elevation,
-        activity_elevation_extension=elevation_extension,
-        prettify=args.prettify,
-    )
+        # We still plot the map as a PNG for thumbnails/socials
+        collection.plot_map(
+            filepath=str(map_path.with_suffix(".png")),
+            linewidths=[16, 8],
+            height=500,
+            width_to_height=1.0,
+            places=places,
+            verbose=verbose,
+        )
+        collection.plot_map(
+            filepath=str(path_static / f"{collection_filename}-map-thick.png"),
+            linewidths=[32, 16],
+            height=500,
+            width_to_height=1.0,
+            places=places,
+            verbose=verbose,
+        )
+
+    if not skip_pages:
+        collection.generate_astro(
+            filepath=str(path_collection_astro),
+            mapfig_name=mapfig_name,
+            elevfig_name=elevfig_name,
+            include_activity_elevation=getattr(
+                args, "include_activity_elevation", True
+            ),
+            activity_elevation_extension=elevation_extension,
+            prettify=getattr(args, "prettify", False),
+        )
 
     if legacy_markdown_path.exists():
         legacy_markdown_path.unlink()
@@ -254,85 +256,10 @@ def generate_collection_from_yaml(
 
 def main():
     """Main method called from the command line."""
-    parser = argparse.ArgumentParser(description="Plot Strava activities.")
-    parser.add_argument(
-        "ids",
-        nargs="*",
-        type=str,
-        help="Space-separated list of Strava activity IDs, e.g., 123 456 789",
+    parser = argparse.ArgumentParser(
+        description="Strava Collections: Analyze, build and serve your adventures.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-
-    parser.add_argument(
-        "-i",
-        "--input",
-        nargs="+",
-        default=None,
-        help="One or more input yaml files or glob patterns (default: None)",
-    )
-
-    parser.add_argument(
-        "-o",
-        "--output",
-        default=None,
-        help=(
-            "Site output directory. When set, a self-contained Astro template is "
-            "copied to <output>/astro and generated collection data is written to "
-            "<output>/source."
-        ),
-    )
-
-    parser.add_argument(
-        "-c",
-        "--collection",
-        default="Example collection",
-        help="Collection name, default: Example collection",
-    )
-
-    parser.add_argument(
-        "-b",
-        "--backend",
-        default="plotly",
-        choices=["plotly", "tikzfigure", "matplotlib"],
-    )
-
-    parser.add_argument(
-        "-f",
-        "--force-update",
-        action="store_true",
-        help="Force update strava activities",
-    )
-
-    parser.add_argument(
-        "-p",
-        "--prettify",
-        action="store_true",
-        help="Prettify generated collection files",
-    )
-    parser.add_argument(
-        "--include-activity-elevation",
-        action=argparse.BooleanOptionalAction,
-        default=True,
-        help="Generate and embed each activity elevation plot in the collection page.",
-    )
-
-    parser.add_argument(
-        "--download",
-        action="store_true",
-        help="Download Strava activity streams to JSON",
-    )
-
-    parser.add_argument(
-        "--serve",
-        action="store_true",
-        help="Install dependencies and start the development server",
-    )
-
-    parser.add_argument(
-        "--host",
-        action="store_true",
-        help="Host the development server",
-    )
-
     parser.add_argument(
         "-v",
         "--verbose",
@@ -340,66 +267,245 @@ def main():
         help="Enable verbose output",
     )
 
+    subparsers = parser.add_subparsers(dest="command", help="Command to execute")
+
+    # Command: build
+    build_parser = subparsers.add_parser(
+        "build", help="Process collections and generate pages/assets"
+    )
+    build_parser.add_argument(
+        "ids",
+        nargs="*",
+        type=str,
+        help="Space-separated list of Strava activity IDs",
+    )
+    build_parser.add_argument(
+        "-i",
+        "--input",
+        nargs="+",
+        help="One or more input YAML files or glob patterns",
+    )
+    build_parser.add_argument(
+        "-c",
+        "--collection",
+        default="Example collection",
+        help="Collection name",
+    )
+    build_parser.add_argument(
+        "-o",
+        "--output",
+        help="Site output directory (overrides YAML output_dir)",
+    )
+    build_parser.add_argument(
+        "-f",
+        "--force-update",
+        action="store_true",
+        help="Force update strava activities",
+    )
+    build_parser.add_argument(
+        "-b",
+        "--backend",
+        default="plotly",
+        choices=["plotly", "tikzfigure", "matplotlib"],
+        help="Elevation plotting backend",
+    )
+    build_parser.add_argument(
+        "-p",
+        "--prettify",
+        action="store_true",
+        help="Prettify generated collection files",
+    )
+    build_parser.add_argument(
+        "--skip-assets",
+        action="store_true",
+        help="Skip generating figures and GPX assets",
+    )
+    build_parser.add_argument(
+        "--skip-pages",
+        action="store_true",
+        help="Skip generating Astro pages",
+    )
+    build_parser.add_argument(
+        "--no-elevation",
+        dest="include_activity_elevation",
+        action="store_false",
+        help="Disable individual activity elevation plots",
+    )
+    build_parser.set_defaults(include_activity_elevation=True)
+
+    # Command: analyze
+    analyze_parser = subparsers.add_parser(
+        "analyze", help="Analyze collections and show statistics without building"
+    )
+    analyze_parser.add_argument(
+        "-i",
+        "--input",
+        nargs="+",
+        required=True,
+        help="One or more input YAML files or glob patterns",
+    )
+    analyze_parser.add_argument(
+        "-f",
+        "--force-update",
+        action="store_true",
+        help="Force update strava activities",
+    )
+
+    # Command: site
+    site_parser = subparsers.add_parser("site", help="Manage the Astro website")
+    site_subparsers = site_parser.add_subparsers(
+        dest="site_command", help="Site sub-command"
+    )
+
+    site_init_parser = site_subparsers.add_parser(
+        "init", help="Initialize/update the Astro template"
+    )
+    site_init_parser.add_argument("-o", "--output", help="Site directory")
+
+    site_sync_parser = site_subparsers.add_parser(
+        "sync", help="Sync generated source files to the Astro site"
+    )
+    site_sync_parser.add_argument("-o", "--output", help="Site directory")
+
+    site_build_parser = site_subparsers.add_parser(
+        "build", help="Run npm build for the Astro site"
+    )
+    site_build_parser.add_argument("-o", "--output", help="Site directory")
+
+    # Command: serve
+    serve_parser = subparsers.add_parser("serve", help="Run the development server")
+    serve_parser.add_argument("-o", "--output", help="Site directory")
+    serve_parser.add_argument(
+        "--host", action="store_true", help="Expose server to host"
+    )
+
+    # Command: activities
+    act_parser = subparsers.add_parser("activities", help="Individual activity tasks")
+    act_parser.add_argument("ids", nargs="+", help="Strava activity IDs")
+    act_parser.add_argument(
+        "--download", action="store_true", help="Download activity streams to JSON"
+    )
+
+    # LEGACY / Shortcut handling
+    if (
+        len(sys.argv) > 1
+        and sys.argv[1] not in subparsers.choices
+        and sys.argv[1] not in ["-h", "--help", "-v", "--verbose"]
+    ):
+        if any(
+            arg in sys.argv
+            for arg in ["-i", "--input", "-o", "--output", "-c", "--collection"]
+        ):
+            sys.argv.insert(1, "build")
+        elif sys.argv[1].isdigit():
+            # If we have other build-related flags but no subcommand, it's a build
+            if any(
+                arg in sys.argv
+                for arg in [
+                    "-b",
+                    "--backend",
+                    "-f",
+                    "--force-update",
+                    "-p",
+                    "--prettify",
+                ]
+            ):
+                sys.argv.insert(1, "build")
+            else:
+                sys.argv.insert(1, "activities")
+
     args = parser.parse_args()
 
-    if args.download:
-        import json
-        from pathlib import Path
+    if getattr(args, "command", None) == "activities":
+        if args.download:
+            import json
 
-        from strava_collections.activity import StravaActivity
+            from strava_collections.activity import StravaActivity
 
-        for aid in args.ids:
-            act = StravaActivity(int(aid))
-            stream = act._activity_stream
-            data = {}
-            for k, v in stream.items():
-                data[k] = v.data
-            out_path = Path("docs/source") / f"strava_{aid}.json"
-            out_path.parent.mkdir(parents=True, exist_ok=True)
-            with open(out_path, "w") as f:
-                json.dump(data, f)
-            print(f"Downloaded {aid} to {out_path}")
+            for aid in args.ids:
+                act = StravaActivity(int(aid))
+                stream = act._activity_stream
+                data = {k: v.data for k, v in stream.items()}
+                out_path = Path("docs/source") / f"strava_{aid}.json"
+                out_path.parent.mkdir(parents=True, exist_ok=True)
+                with open(out_path, "w") as f:
+                    json.dump(data, f)
+                print(f"Downloaded {aid} to {out_path}")
+        return
+
+    if getattr(args, "command", None) == "analyze":
+        input_paths = resolve_input_paths(args.input)
+        for input_path in input_paths:
+            with open(input_path, "r", encoding="utf-8") as f:
+                data = yaml.safe_load(f)
+            StravaCollection(
+                name=data["collection_name"],
+                activities=data.get("activities") or data.get("activity_ids", []),
+                force_update=args.force_update,
+                verbose=args.verbose,
+                description=data.get("description"),
+                places=data.get("places"),
+            )
+        return
+
+    if getattr(args, "command", None) == "site":
+        site_root = (
+            Path(args.output).resolve() if args.output else Path("docs").resolve()
+        )
+        if args.site_command == "init":
+            print(f"Initializing/Updating site at: {site_root}")
+            ensure_site_template(site_root)
+        elif args.site_command == "sync":
+            print(f"Syncing site at: {site_root}")
+            sync_site(site_root)
+        elif args.site_command == "build":
+            astro_dir = site_root / "astro"
+            print(f"Building site at: {astro_dir}")
+            subprocess.run(
+                ["npm", "run", "build:from-generated"], cwd=astro_dir, check=True
+            )
         return
 
     site_root: Path | None = None
-    if args.input:
-        input_paths = resolve_input_paths(args.input)
-        for input_path in input_paths:
-            site_root = generate_collection_from_yaml(input_path=input_path, args=args)
-    else:
-        output_dir, site_root = resolve_output_directory(args)
-        generate_collection(
-            collection_name=args.collection,
-            activities=args.ids,
-            output_dir=output_dir,
-            fallback_static_dir=None,
-            args=args,
-        )
-    if args.verbose:
-        print(f"Generated collection with activities: {args.ids}")
+    if getattr(args, "command", None) == "build":
+        if args.input:
+            input_paths = resolve_input_paths(args.input)
+            for input_path in input_paths:
+                site_root = generate_collection_from_yaml(
+                    input_path=input_path, args=args, verbose=args.verbose
+                )
+        elif args.ids:
+            output_dir, site_root = resolve_output_directory(args)
+            generate_collection(
+                collection_name=args.collection,
+                activities=args.ids,
+                output_dir=output_dir,
+                fallback_static_dir=None,
+                args=args,
+                verbose=args.verbose,
+            )
 
-    if site_root is None:
-        print("No site generated since no output directory was specified.")
-        return
+        if site_root is None:
+            print("No site generated since no output directory was specified.")
+            return
 
-    if site_root is not None:
         sync_site(site_root)
-        import os
-        import shutil
-
         if os.path.exists("live-tracking.yaml"):
+            import shutil
+
             shutil.copy(
                 "live-tracking.yaml", site_root / "source" / "live-tracking.yaml"
             )
         print_site_instructions(site_root)
 
-    if args.serve and site_root is not None:
+    if getattr(args, "command", None) == "serve":
+        site_root = (
+            Path(args.output).resolve() if args.output else Path("docs").resolve()
+        )
         astro_dir = site_root / "astro"
         print(f"\n🚀 Starting development server in {astro_dir}...")
         try:
-            # Install dependencies
             subprocess.run(["npm", "ci"], cwd=astro_dir, check=True)
-            # Start dev server
             npm_command = ["npm", "run", "dev"]
             if args.host:
                 npm_command.extend(["--", "--host"])
